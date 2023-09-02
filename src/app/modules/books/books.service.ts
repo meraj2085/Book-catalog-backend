84daf5210a7f3b22c013e20f793df854924ffd61
Meraj Hossain <merajhossain@Merajs-MacBook-Air.local>
2023-09-02 09:26:40 +0600

    Adding books

--- src/app/modules/books/books.service.ts
+++ src/app/modules/books/books.service.ts
@@ -1,6 +1,7 @@
-import { Book, Category } from '@prisma/client';
+import { Book, Category, Prisma } from '@prisma/client';
 import prisma from '../../../shared/prisma';
 import { paginationHelpers } from '../../../helpers/paginationHelper';
+import { booksSearchableFields } from './books.constant';
 
 const createBook = async (data: Book) => {
   const book = await prisma.book.create({
@@ -9,10 +10,59 @@ const createBook = async (data: Book) => {
   return book;
 };
 
-const getAllBooks = async (filter, options) => {
-  // const { page, limit, skip } = paginationHelpers.calculatePagination(options);
-  const books = await prisma.book.findMany({});
-  return books;
+const getAllBooks = async (filter: any, options: any) => {
+  const { page, limit, skip } = paginationHelpers.calculatePagination(options);
+  const { search, ...filterData } = filter;
+  const andConditions = [];
+
+  if (search) {
+    andConditions.push({
+      OR: booksSearchableFields.map(field => ({
+        [field]: {
+          contains: search,
+          mode: 'insensitive',
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map(key => ({
+        [key]: {
+          equals: (filterData as any)[key],
+        },
+      })),
+    });
+  }
+
+  const whereConditions: Prisma.BookWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+
+  const result = await prisma.book.findMany({
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? {
+            [options.sortBy]: options.sortOrder,
+          }
+        : {
+            createdAt: 'desc',
+          },
+  });
+
+  const total = await prisma.book.count();
+
+  return {
+    meta: {
+      total,
+      page,
+      limit,
+    },
+    data: result,
+  };
 };
 
 const getBooksByCategory = async (id: string) => {
